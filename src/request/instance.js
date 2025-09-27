14a5d2b8fd1b18fb186dff5243b0490752f83170
Natanael Abad <natanaelabaad@gmail.com>
2025-09-26 23:19:48 -0400

    Filtro para incluir las fotos de los miembros

--- src/request/instance.js
+++ src/request/instance.js
@@ -5,7 +5,7 @@ import router from "../router";
 import { convertToBase64 } from "../utils/inforFormat";
 
 const Requester = axios.create({
-    baseURL: process.env.NODE_ENV === "production" ? window.location.origin : "http://192.168.51.153:81"
+    baseURL: process.env.NODE_ENV === "production" ? window.location.origin : "http://192.168.1.20:81"
 });
 
 Requester.interceptors.response.use((response) => response, (err) => {
@@ -200,8 +200,8 @@ const axiosRequest = {
             let form = await Requester.get(`/api/reporter/print/member?id=${id}`, { responseType: 'blob', headers: { Authorization: "*" } });
             return form;
         },
-        generateMembersListForm: async (filters) => {
-            let form = await Requester.get(`/api/reporter/print/members${filters ? `?filters=${convertToBase64(JSON.stringify(filters))}`: ''}`, { responseType: 'blob', headers: { Authorization: "*" } });
+        generateMembersListForm: async (filters, includePhotos) => {
+            let form = await Requester.get(`/api/reporter/print/members?includePhotos=${includePhotos}${filters ? `&filters=${convertToBase64(JSON.stringify(filters))}`: ''}`, { responseType: 'blob', headers: { Authorization: "*" } });
             return form;
         },
         LevelResume: async () => {
